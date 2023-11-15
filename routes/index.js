311faa8beb7abe2e06c2ee478a5efc4e8b00c98b
barbabianca <ukasikcioglu@yahoo.com>
2023-11-15 10:36:46 +0530

    secondcommit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const { createUser,loginUser, getUsers } = require('../controllers/userController')
-
 const router = express.Router()
-
 const passport = require('passport')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix+file.originalname.trim())
+    }
+  })
+  
+  const upload = multer({ storage })
+
 
 
 router.post('/create-user', createUser)
@@ -12,5 +24,13 @@ router.post('/login-user', loginUser)
 
 router.get('/get-users',passport.authenticate('jwt', {session:false}), getUsers)  
 
+router.post('/upload', upload.single('banner'), (req, res) => {
+    console.log(req.file);
+    return res.status(200).json({
+        statusCode: 200,
+        message: 'File uploaded successfully'
+    });
+
+  });
 
 module.exports = router
